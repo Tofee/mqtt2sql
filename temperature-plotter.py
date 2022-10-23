1dc57ad28a18fe43dad18abc882fc5bf47f48b28
Christophe Chapuis <chris.chapuis@gmail.com>
2022-10-23 20:05:35 +0200

    Improve temperature plotter

--- temperature-plotter.py
+++ temperature-plotter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import sys
 import json
 import sqlite3
 import matplotlib
@@ -11,10 +12,9 @@ from dateutil import parser
 from matplotlib import style
 from matplotlib.dates import date2num
 
-def graph_data():
+def graph_data(sqlite_file, image_path):
     # Connect to database
     now = datetime.now()
-    sqlite_file = '/var/spool/mqtt2sql/mqtt-sensors.db'
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
     c.execute('SELECT ts,value FROM mqtt_history')
@@ -37,11 +37,11 @@ def graph_data():
     # Convert datetime.datetime to float days since 0001-01-01 UTC.
     dates = [date2num(t) for t in timenow]
 
-    style.use('fivethirtyeight')
+    style.use('bmh')
     
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    ax1.set_title("Température du salon")
+    ax1.set_title("Température du salon - battery: {}%".format(battery[-1]))
 
     # Configure x-ticks
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%d.%m.%Y %H:%M'))
@@ -49,10 +49,12 @@ def graph_data():
     # Plot temperature data on left Y axis
     ax1.set_ylabel("Temperature [°C]")
     ax1.plot_date(dates, temperature, '-', label="Temperature", color='r')
+    ax1.yaxis.label.set_color('r')
 
     # Plot humidity data on right Y axis
     ax2 = ax1.twinx()
     ax2.set_ylabel("Humidité [% RH]")
+    ax2.yaxis.label.set_color('b')
     ax2.plot_date(dates, humidity, '-', label="Humidity", color='b')
 
     # Format the x-axis for dates (label formatting, rotation)
@@ -61,12 +63,20 @@ def graph_data():
 
     # Show grids and legends
     ax1.grid(True)
-    ax1.legend(loc='best', framealpha=0.5)
-    ax2.legend(loc='best', framealpha=0.5)
+    ax2.grid(False)
 
-    plt.savefig("/tmp/temperature-salon.png")
+    plt.savefig(image_path, transparent=True)
     
     c.close()
     conn.close()
-    
-graph_data()
+
+database_path = '/var/spool/mqtt2sql/mqtt-sensors.db'
+output_image = '/tmp/temperature-salon.png'
+
+if len(sys.argv) == 3:
+    database_path = sys.argv[1]
+    output_image = sys.argv[2]
+
+print("Updating temperature plot in {} using {}...".format(output_image, database_path))
+graph_data(database_path, output_image)
+
